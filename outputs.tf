d39b687d33e455b1890c2bbc67c2bb5da2d6bda0
Colin Wilson <colin.wilson@protonmail.ch>
2020-12-17 05:03:44 +0000

    🐛 fix output value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vault_service_config_name" {
   description = "Name of generated Vault configuration file"
-  value       = vault_config.vault_hcl.name
+  value       = docker_config.vault_hcl.name
 }
